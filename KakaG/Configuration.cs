ea30d7faaea21742ef4af1525ed1de6b6367591f
Totoki Kei <totoki_kei@outlook.com>
2014-04-12 18:11:39 +0900

    Add HP Restriction, and configuration file

---
+++ KakaG/Configuration.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace MackerelPluginSet.KakaG {
+	[Serializable]
+	public class Configuration {
+
+		[Serializable]
+		public class GodModeConfiguration {
+			public bool ObsidianSkinEnabled { get; set; }
+			public bool PvPfragileEnabled { get; set; }
+		}
+		[Serializable]
+		public class HPRequirementConfiguration {
+			public int RequiredHP { get; set; }
+			public string KickMessageFormat { get; set; }
+		}
+
+		public GodModeConfiguration GodMode;
+		public HPRequirementConfiguration HPRequirement;
+
+
+		[XmlIgnore]
+		public static readonly string ConfigurationFileName = "KakaG.xml";
+
+		public Configuration() {
+			// デフォルト値の設定
+			GodMode = new GodModeConfiguration() { ObsidianSkinEnabled = true, PvPfragileEnabled = true };
+			HPRequirement = new HPRequirementConfiguration() { RequiredHP = 400, KickMessageFormat = "Need HP {0} over. your HP is {1}." };
+		}
+
+		public static Configuration Load() {
+			XmlSerializer serializer = new XmlSerializer(typeof(Configuration));
+			Configuration cfg = null;
+			if (!File.Exists(ConfigurationFileName)) {
+				// ファイルがない
+
+				cfg = new Configuration(); // 初期値
+				try {
+					// 初期値のファイルを作成
+					using (var f = File.OpenWrite(ConfigurationFileName)) {
+						serializer.Serialize(f, cfg);
+					}
+				}
+				catch (IOException) {
+					// にぎりつぶす
+				}
+			}
+			else {
+				try {
+					using (var f = File.OpenRead(ConfigurationFileName)) {
+						cfg = serializer.Deserialize(f) as Configuration;
+					}
+				}
+				catch (IOException) {
+					// デフォルト値を返す
+					cfg = new Configuration();
+				}
+
+			}
+
+			return cfg;
+
+		}
+	}
+}
--- KakaG/Plugin.cs
+++ KakaG/Plugin.cs
@@ -8,45 +8,59 @@ using Terraria;
 using TerrariaApi.Server;
 using TS = TShockAPI;
 
-namespace MackerelPluginSet.KagaG {
+namespace MackerelPluginSet.KakaG {
 
 	[ApiVersion(1, 15)]
 	public class Plugin : TerrariaPlugin {
+		Configuration conf;
 
 		public Plugin(Main game)
 			: base(game) {
+
+			conf = Configuration.Load();
 		}
 
-		
+
 
 		public override void Initialize() {
 
-			if (TS.GetDataHandlers.PlayerHP == null) {
-				TS.GetDataHandlers.PlayerHP = new TS.HandlerList<TS.GetDataHandlers.PlayerHPEventArgs>();
-			}
-			if (TS.GetDataHandlers.PlayerDamage == null) {
-				TS.GetDataHandlers.PlayerDamage = new TS.HandlerList<TS.GetDataHandlers.PlayerDamageEventArgs>();
-			}
 
 
-			{
+			if (conf.GodMode.ObsidianSkinEnabled) {
+				if (TS.GetDataHandlers.PlayerHP == null) {
+					TS.GetDataHandlers.PlayerHP = new TS.HandlerList<TS.GetDataHandlers.PlayerHPEventArgs>();
+				}
 				var handler = new TS.HandlerList<TS.GetDataHandlers.PlayerHPEventArgs>.HandlerItem();
 				handler.Handler = OnHp;
 				handler.Priority = 0;
 
 				TS.GetDataHandlers.PlayerHP.Register(handler);
 			}
-			{
+
+			if (conf.GodMode.PvPfragileEnabled) {
+				if (TS.GetDataHandlers.PlayerDamage == null) {
+					TS.GetDataHandlers.PlayerDamage = new TS.HandlerList<TS.GetDataHandlers.PlayerDamageEventArgs>();
+				}
 				var handler = new TS.HandlerList<TS.GetDataHandlers.PlayerDamageEventArgs>.HandlerItem();
 				handler.Handler = OnDamage;
 				handler.Priority = 0;
 
 				TS.GetDataHandlers.PlayerDamage.Register(handler);
 			}
-
+			if (conf.HPRequirement.RequiredHP != 0) {
+				ServerApi.Hooks.ServerJoin.Register(this, ServerHooks_Join);
+			}
 
 			TS.Log.ConsoleInfo("Mackerel KakaG Plugin is loaded.");
 
+		}
+
+		private void ServerHooks_Join(JoinEventArgs args) {
+			var player = TS.TShock.Players[args.Who];
+			if (player.FirstMaxHP < conf.HPRequirement.RequiredHP) {
+				string msg = string.Format(conf.HPRequirement.KickMessageFormat, conf.HPRequirement.RequiredHP, player.FirstMaxHP);
+				TS.TShock.Utils.Kick(player, msg, silent: true);
+			}
 		}
 
 		private void OnDamage(object sender, TS.GetDataHandlers.PlayerDamageEventArgs e) {
@@ -61,21 +75,9 @@ namespace MackerelPluginSet.KagaG {
 				// あいだをおいて ころす
 				Task t = new Task(() => { Thread.Sleep(3000); player.DamagePlayer(9999); });
 				t.Start();
-				
+
 			}
 		}
-
-		//private void OnPlayerUpdate(object sender, TS.GetDataHandlers.PlayerUpdateEventArgs e) {
-		//	//e.p
-		//}
-
-		//private void OnTogglePvp(object sender, TS.GetDataHandlers.TogglePvpEventArgs e) {
-		//	TS.TSPlayer player = TS.TShock.Players[e.PlayerId];
-
-		//	if (player.GodMode) {
-		//		//e.
-		//	}
-		//}
 
 		private void OnHp(object sender, TS.GetDataHandlers.PlayerHPEventArgs e) {
 			// このハンドラが呼ばれるのは、GodModeの回復判定などが行われるときと同じタイミング。
@@ -88,7 +90,7 @@ namespace MackerelPluginSet.KagaG {
 		}
 
 		public override Version Version {
-			get { return new Version("1.1"); }
+			get { return new Version("1.2"); }
 		}
 		public override string Name {
 			get { return "Mackerel KakaG Plugin"; }
