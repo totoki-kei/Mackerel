3d72cd3effe808e057fb800b3377c9987e457b9c
totoki_kei@outlook.com <totoki_kei@outlook.com>
2015-09-05 10:45:16 +0900

    Add NoJP Plugin

---
+++ FuckinJap/Plugin.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using Terraria;
+using TerrariaApi.Server;
+using TS = TShockAPI;
+
+namespace MackerelPluginSet.NoJP {
+
+	//*
+	[ApiVersion(1, 21)]
+	public class Plugin : TerrariaPlugin {
+
+		[Serializable]
+		public class Configuration {
+			public bool Enabled = false;
+			public string LoginKickMessage = "this server is not for you.";
+			public string ChatKickMessage = "this server is not for you.";
+
+			[XmlIgnore]
+			public static readonly string ConfigurationFileName = "NoJP.xml";
+
+			public Configuration() { }
+
+			public static Configuration Load() {
+				XmlSerializer serializer = new XmlSerializer(typeof(Configuration));
+				Configuration cfg = null;
+				if (!File.Exists(ConfigurationFileName)) {
+					// ファイルがない
+
+					cfg = new Configuration(); // 初期値
+					try {
+						// 初期値のファイルを作成
+						using (var f = File.OpenWrite(ConfigurationFileName)) {
+							serializer.Serialize(f, cfg);
+						}
+					}
+					catch (IOException) {
+						// にぎりつぶす
+					}
+				}
+				else {
+					try {
+						using (var f = File.OpenRead(ConfigurationFileName)) {
+							cfg = serializer.Deserialize(f) as Configuration;
+						}
+					}
+					catch (IOException) {
+						// デフォルト値を返す
+						cfg = new Configuration();
+					}
+
+				}
+
+				return cfg;
+
+			}
+
+
+		}
+
+		Configuration config;
+
+		public Plugin(Main game)
+			: base(game) {
+		}
+
+		public override void Initialize() {
+			config = Configuration.Load();
+
+			ServerApi.Hooks.ServerJoin.Register(this, ServerHooks_Join);
+			ServerApi.Hooks.ServerChat.Register(this, ServerHooks_Chat);
+			TS.Hooks.GeneralHooks.ReloadEvent += GeneralHooks_ReloadEvent;
+
+			TS.TShock.Log.ConsoleInfo("Mackerel NoJP Plugin is loaded.");
+
+		}
+
+		void GeneralHooks_ReloadEvent(TS.Hooks.ReloadEventArgs e) {
+			config = Configuration.Load();
+		}
+
+		private void ServerHooks_Chat(ServerChatEventArgs args) {
+			if (!config.Enabled) return;
+
+			var player = TS.TShock.Players[args.Who];
+
+			if (Judge(args.Text)) {
+				player.Disconnect(config.ChatKickMessage);
+			}
+		}
+
+		private void ServerHooks_Join(JoinEventArgs args) {
+			if (!config.Enabled) return;
+
+			var player = TS.TShock.Players[args.Who];
+
+			if (Judge(player.Name)) {
+				player.Disconnect(config.LoginKickMessage);
+			}
+		}
+
+		private static bool Judge(string s) {
+			return s.Any(c => (int)c > 0x7f);
+		}
+
+		public override Version Version {
+			get { return new Version("1.0"); }
+		}
+		public override string Name {
+			get { return "Mackerel NoJP Plugin"; }
+		}
+		public override string Author {
+			get { return "Totoki Kei"; }
+		}
+		public override string Description {
+			get { return "********************"; }
+		}
+	}
+	//*/
+}
--- mackereler/Cooltime/Plugin.cs
+++ mackereler/Cooltime/Plugin.cs
@@ -1,36 +1,79 @@
 ﻿using System;
-
-using TS = TShockAPI;
 using Terraria;
-using System.Collections.Generic;
+using TerrariaApi.Server;
+using TS = TShockAPI;
 
-namespace MackerelPluginSet {
-#if false
-	[APIVersion(1, 12)]
-	public class Plugin : TerrariaPlugin {
+namespace MackerelPluginSet.Cooltime {
+	//*
+		[ApiVersion(1, 21)]
+		public class Plugin : TerrariaPlugin {
 
-		public Plugin(Main game)
-			: base(game) {	
-		}
+			public Plugin(Main game) : base(game) {
 
-		public override void Initialize() {
+				// TShockに優先してフックする
+				this.Order = -1;
+			}
 
-			TS.TShock.Log.ConsoleInfo("Mackerel Cooltime Plugin is loaded.");
+			public override void Initialize() {
+				TS.TShock.Log.ConsoleInfo("Mackerel Cooltime Plugin is loaded.");
 
-		}
+				ServerApi.Hooks.NetGetData.Register(this, OnGetData);
+			}
 
-		public override Version Version {
-			get { return new Version("1.0"); }
+			protected override void Dispose(bool disposing) {
+				if (disposing) ServerApi.Hooks.NetGetData.Deregister(this, OnGetData);
+				base.Dispose(disposing);
+			}
+
+			private void OnGetData(GetDataEventArgs e) {
+				// バイナリ解析部分はTShockソースから拝借
+				if (e.Handled)
+					return;
+
+				PacketTypes type = e.MsgID;
+				var player = TS.TShock.Players[e.Msg.whoAmI];
+
+				if (player == null || !player.ConnectionAlive) {
+					return;
+				}
+				if (player.RequiresPassword && type != PacketTypes.PasswordSend) {
+					return;
+				}
+				if ((player.State < 10 || player.Dead) 
+					&& (int)type > 12 
+					&& (int)type != 16 
+					&& (int)type != 42 
+					&& (int)type != 50 
+					&& (int)type != 38 
+					&& (int)type != 21
+					&& (int)type != 22) {
+					return;
+				}
+
+				if (type == PacketTypes.SpawnBossorInvasion) {
+					var spawnboss = false;
+					var invasion = false;
+					var plr = e.Data.ReadInt16();
+					var Type = e.Data.ReadInt16();
+					NPC npc = new NPC();
+					npc.SetDefaults(Type);
+					spawnboss = npc.boss;
+
+				}
+			}
+
+			public override Version Version {
+				get { return new Version("1.0"); }
+			}
+			public override string Name {
+				get { return "Mackerel Plugin"; }
+			}
+			public override string Author {
+				get { return "Totoki Kei"; }
+			}
+			public override string Description {
+				get { return "Apply summon cooltime"; }
+			}
 		}
-		public override string Name {
-			get { return "Mackerel Cooltime Plugin"; }
-		}
-		public override string Author {
-			get { return "Totoki Kei"; }
-		}
-		public override string Description {
-			get { return "Plugin suite"; }
-		}
-	}
-#endif
+	//*/
 }
